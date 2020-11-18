2bb4a2c9ff0a3d705292be8a4b835a2f9a51b793
James Costello <jmc@Jamess-MacBook-Pro.local>
2020-11-18 12:17:34 -0700

    questions/answers

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 'use strict';
 
-const fs = require('fs');
-const generatePage = require('./src/page-temp.js');
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const [name, github] = profileDataArgs;
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-temp.js');
+
+// const pageHTML = generatePage(name, github);
 
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
-  console.log('Portfolio Complete! Check out index.html to see the output!');
-});
+// fs.writeFile('index.html', pageHTML, err => {
+//   if (err) throw err;
+
+//   console.log('Portfolio Complete! Check out index.html to see the output!');
+// });
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name',
+    }
+  ])
+  .then(answers => console.log(answers));
